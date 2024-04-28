58aced6507fa32175a49747c0996594aed34d314
CloverHsc <clover.hsc@gmail.com>
2024-04-28 15:55:07 +0800

    Input property transformation. Video 7

--- src/app/Counter/counter.component.ts
+++ src/app/Counter/counter.component.ts
@@ -1,15 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, booleanAttribute } from '@angular/core';
 import { multiple10, trimMsg } from './counter.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.scss'
 })
 export default class CounterComponent {
   @Input({ required: true, transform: multiple10 }) count = 0;
   @Input({ transform: trimMsg }) message = '';
+  @Input({ transform: booleanAttribute }) showcountmsg = false;
 }
 
 
